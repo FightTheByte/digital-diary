a796a800023f5e162ae84b5996be047d135afeef
Martin Cameron <highlandrocketofficial@gmail.com>
2025-09-25 00:06:33 +0100

    init read posts route

--- code/digital-diary/src/index.js
+++ code/digital-diary/src/index.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 import {Landing} from '../src/components/landing-page';
 import { CreatePost } from './components/create-post';
+import { ReadPosts } from './components/read-page';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,6 +16,7 @@ root.render(
     <Routes>
       <Route path='/' element={<Landing/>}></Route>
       <Route path='/post' element={<CreatePost/>}></Route>
+      <Route path='/read' element={<ReadPosts/>}></Route>
     </Routes>
   </Router>
 );
