28d403f9237d0ceaa2404afbccddb899b81cefc2
Martin Cameron <highlandrocketofficial@gmail.com>
2025-09-24 16:29:07 +0100

    add create post route

--- code/digital-diary/src/index.js
+++ code/digital-diary/src/index.js
@@ -6,15 +6,15 @@ import {
   Route
 } from 'react-router-dom';
 import {Landing} from '../src/components/landing-page';
+import { CreatePost } from './components/create-post';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Routes>
-      <Route path='/' element={<Landing/>}>
-
-      </Route>
+      <Route path='/' element={<Landing/>}></Route>
+      <Route path='/post' element={<CreatePost/>}></Route>
     </Routes>
   </Router>
 );
