19aa811ad833364189a20078c27c479c882af44c
Martin Cameron <highlandrocketofficial@gmail.com>
2025-09-25 00:56:55 +0100

    init read page code

--- code/digital-diary/src/components/read-page.js
+++ code/digital-diary/src/components/read-page.js
@@ -1,10 +1,44 @@
+import { useEffect, useState } from "react";
+import '../styles/read-page.css'
 
 export const ReadPosts = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        async function getPosts(){
+            const response = await fetch('http://localhost:4000/get-posts', {
+                method: 'GET',
+                credentials: 'include'
+            })
+            const jsonResponse = await response.json();
+            setPosts(jsonResponse.response);
+        }
+        getPosts();
+    }, [])
 
     return(
         <>
-            <div>
-                <p>here</p>
+            <div 
+                className="read-post"
+                style={{
+                    display: "grid",
+                    gridTemplateRows: `repeat(${posts.length}, 1fr)`
+                }}
+            >
+                {
+                    posts
+                    ?posts.map((element, index) => {
+                        return <div 
+                            key={element.id}
+                            className="posts"
+                        >
+                            <h4>{element.title}</h4>
+                            <p>{new Date(element.date).toUTCString()}</p>
+                            <div className="delete">x</div>
+                        </div>
+                    })
+                    :null
+                }
             </div>
         </>
     )
