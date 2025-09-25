be508258d8d6047da4727076ab1ef3cca8cb477d
Martin Cameron <highlandrocketofficial@gmail.com>
2025-09-25 22:28:54 +0100

    added account to routing

--- code/digital-diary/src/index.js
+++ code/digital-diary/src/index.js
@@ -8,6 +8,7 @@ import {
 import {Landing} from '../src/components/landing-page';
 import { CreatePost } from './components/create-post';
 import { ReadPosts } from './components/read-page';
+import { Account } from './components/account-page';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -17,6 +18,7 @@ root.render(
       <Route path='/' element={<Landing/>}></Route>
       <Route path='/post' element={<CreatePost/>}></Route>
       <Route path='/read' element={<ReadPosts/>}></Route>
+      <Route path='/account' element={<Account/>}></Route>
     </Routes>
   </Router>
 );
