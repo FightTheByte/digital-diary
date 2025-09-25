b092b0ce3e8cf34c2fb7c9a5e9e92ba5a9ae93e1
Martin Cameron <highlandrocketofficial@gmail.com>
2025-09-25 14:02:47 +0100

    added formatting

--- code/digital-diary/src/components/read-page.js
+++ code/digital-diary/src/components/read-page.js
@@ -1,45 +1,58 @@
 import { useEffect, useState } from "react";
-import '../styles/read-page.css'
+import "../styles/read-page.css";
 
 export const ReadPosts = () => {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        async function getPosts(){
-            const response = await fetch('http://localhost:4000/get-posts', {
-                method: 'GET',
-                credentials: 'include'
-            })
-            const jsonResponse = await response.json();
-            setPosts(jsonResponse.response);
-        }
-        getPosts();
-    }, [])
+  useEffect(() => {
+    async function getPosts() {
+      const response = await fetch("http://localhost:4000/get-posts", {
+        method: "GET",
+        credentials: "include",
+      });
+      const jsonResponse = await response.json();
+      setPosts(jsonResponse.response);
+    }
+    getPosts();
+  }, []);
 
-    return(
-        <>
-            <div 
-                className="read-post"
-                style={{
-                    display: "grid",
-                    gridTemplateRows: `repeat(${posts.length}, 1fr)`
-                }}
-            >
-                {
-                    posts
-                    ?posts.map((element, index) => {
-                        return <div 
-                            key={element.id}
-                            className="posts"
+  return (
+    <>
+      <div className="read-background">
+        <div className="read-container">
+          <div className="read-book">
+            <div className="read-binder"></div>
+                <div className="read-page">
+                    <div className="post-layout">
+                        <div className="search-tools"><p>tools</p></div>
+                        <div
+                          className="read-post"
+                          style={{
+                            display: "grid",
+                            gridTemplateRows: `repeat(${posts.length}, 1fr)`,
+                          }}
                         >
-                            <h4>{element.title}</h4>
-                            <p>{new Date(element.date).toUTCString()}</p>
-                            <div className="delete">x</div>
+                          {posts
+                            ? posts.map((element, index) => {
+                                return (
+                                  <div className="post-container">
+                                    <div className="tabs"></div>
+                                    <div key={element.id} className="posts">
+                                      <h4>{element.title}</h4>
+                                      <p>{new Date(element.date).toUTCString()}</p>
+                                      <div className="delete">x</div>
+                                    </div>
+                                  </div>
+                                );
+                              })
+                            : null}
                         </div>
-                    })
-                    :null
-                }
-            </div>
-        </>
-    )
-}
+                        <div className="write-icon"><p>write</p></div>
+                    </div>
+                </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
