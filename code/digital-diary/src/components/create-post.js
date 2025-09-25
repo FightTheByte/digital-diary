d17c398967ff780e117630bef6dc53982c6f83a5
Martin Cameron <highlandrocketofficial@gmail.com>
2025-09-25 16:52:50 +0100

    fixed formatting and styling error

--- code/digital-diary/src/components/create-post.js
+++ code/digital-diary/src/components/create-post.js
@@ -1,138 +1,131 @@
-import '../styles/post-page.css';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import bookmark from '../assets/bookmark.png';
+import "../styles/post-page.css";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import bookmark from "../assets/bookmark.png";
 
 export const CreatePost = () => {
-    const [binderArray, setBinderArray] = useState(Array(6));
-    const [body, setBody] = useState(false);
-    const [title, setTitle] = useState(false);
-    const [tags, setTags] = useState(false);
-    const navigate = useNavigate();
+  const [binderArray, setBinderArray] = useState(Array(6));
+  const [body, setBody] = useState(false);
+  const [title, setTitle] = useState(false);
+  const [tags, setTags] = useState(false);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        async function populateBinder(){
-            let holder = Array.from({length: 6},(element, index) => {
-                return <div
-                    className='binder-holes'
-                ></div>
-            })
-            setBinderArray(holder)
-        }
-        populateBinder();
-    }, [])
+  useEffect(() => {
+    async function populateBinder() {
+      let holder = Array.from({ length: 6 }, (element, index) => {
+        return <div className="binder-holes"></div>;
+      });
+      setBinderArray(holder);
+    }
+    populateBinder();
+  }, []);
 
-    useEffect(() => {
-        async function authenticated(){
-            const auth = await fetch('http://localhost:4000/authenticated', {
-                method: "GET",
-                credentials: "include"
-            })
-            const jsonResponse = await auth.json();
+  useEffect(() => {
+    async function authenticated() {
+      const auth = await fetch("http://localhost:4000/authenticated", {
+        method: "GET",
+        credentials: "include",
+      });
+      const jsonResponse = await auth.json();
 
-            if(jsonResponse != true){
-                navigate('/');
-            }
-        }
-        authenticated()
-    }, [])
+      if (jsonResponse != true) {
+        navigate("/");
+      }
+    }
+    authenticated();
+  }, []);
 
-    async function post(){
-        let requestBody;
-        if(body && title && tags){
-            requestBody = {
-                title: title,
-                post: body,
-                tags, tags
-            }
-        }
-
-        if(body && title && !tags){
-            requestBody = {
-                title: title,
-                post: body
-            }
-        }
-        console.log(requestBody)
-        const response = await fetch('http://localhost:4000/post', {
-            method: "POST",
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include',
-            body: JSON.stringify(requestBody)
-        })
-        console.log(response.status);
-        if(response.status == 200){
-            window.location.reload(true);
-        }
+  async function post() {
+    let requestBody;
+    if (body && title && tags) {
+      requestBody = {
+        title: title,
+        post: body,
+        tags,
+        tags,
+      };
     }
 
-    return(
-        <>
-            <div className="post-background">
-                <div className='post-container'>
-                    <div className="post-book">
-                        <img 
-                            src={bookmark}
-                            onClick={() => {
-                                navigate('/read')
-                            }}
-                        />
-                       <div id="post-binder">
-                           {
-                             binderArray
-                           }
-                       </div>
-                       <div id="post-page">
-                           <textarea
-                               maxLength={2000}
-                               minLength={1}
-                               resize="none"
-                               placeholder='Today I....'
-                               onChange={(e) => {
-                                   setBody(e.target.value);
-                               }}
-                           ></textarea>
-                       </div>
-                    </div>
-                </div>
-                <input 
-                    type="text"
-                    maxLength={255}
-                    placeholder='Title'
-                    className="post-input"
-                    onChange={(e) => {
-                        setTitle(e.target.value);
-                    }}
-                >
-                </input>
-                <input 
-                    type="text"
-                    placeholder='Tags: Happy Sad Software'
-                    className="post-input"
-                    onChange={(e) => {
-                        let list = e.target.value
-                        list = list.split(" ");
-                        if(list.length > 10){
-                            alert("Maximum of ten tags")
-                        } else {
-                            setTags(list);
-                        }
-                        
-                    }}
-                >
-                </input>
-                <div>
-                    <button
-                        className='post-button'
-                        onClick={() => {
-                            post();
-                        }}
-                    >Save
-                    </button>
-                </div>
+    if (body && title && !tags) {
+      requestBody = {
+        title: title,
+        post: body,
+      };
+    }
+    console.log(requestBody);
+    const response = await fetch("http://localhost:4000/post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(requestBody),
+    });
+    console.log(response.status);
+    if (response.status == 200) {
+      window.location.reload(true);
+    }
+  }
+
+  return (
+    <>
+      <div className="post-background">
+        <div className="post-container">
+          <div className="post-book">
+            <img
+              src={bookmark}
+              onClick={() => {
+                navigate("/read");
+              }}
+            />
+            <div id="post-binder">{binderArray}</div>
+            <div id="post-page">
+              <textarea
+                maxLength={2000}
+                minLength={1}
+                resize="none"
+                placeholder="Today I...."
+                onChange={(e) => {
+                  setBody(e.target.value);
+                }}
+              ></textarea>
             </div>
-        </>
-    )
-}
+          </div>
+        </div>
+        <div className="input-container">
+            <input
+              type="text"
+              maxLength={255}
+              placeholder="Title"
+              className="post-input"
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            ></input>
+            <input
+              type="text"
+              placeholder="Tags: Happy Sad Software"
+              className="post-input"
+              onChange={(e) => {
+                let list = e.target.value;
+                list = list.split(" ");
+                if (list.length > 10) {
+                  alert("Maximum of ten tags");
+                } else {
+                  setTags(list);
+                }
+              }}
+            ></input>
+            <button
+                className="post-button"
+                onClick={() => {
+                  post();
+                }}
+              >
+                Save
+            </button>
+        </div>
+      </div>
+    </>
+  );
+};
